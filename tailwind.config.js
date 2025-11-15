271845fb323dc3b40c1596b83c53cf87fde257e5
ruu2023 <kokihiru2038@gmail.com>
2025-11-15 19:39:48 +0900

    tailwindSetting

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Reactプロジェクトの場合の一般的な設定
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 /// <reference types="vitest"/>
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 import { defineConfig as defineVitestConfig } from 'vitest/config';
 
 export default defineVitestConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
   test: {
     globals: true,
     environment: 'happy-dom',
